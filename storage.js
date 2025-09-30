aa282665052a78268e4d08bdb6eb98d40c307246
Sam <Doomzfire@hotmail.com>
2025-09-30 00:23:22 -0400

    Update storage.js

--- storage.js
+++ storage.js
@@ -111,7 +111,7 @@ class PgStore {
     return r.rows[0]?.value || 0;
   }
   async applyDelta(user, delta){
-    const d = Math.round(Number(delta));
+    const d = Number(delta);
     const r = await this.pool.query(
       `insert into karma (user_name, value) values ($1, greatest(-5, least(5, $2)))
        on conflict (user_name) do update set value = greatest(-5, least(5, karma.value + $2))
